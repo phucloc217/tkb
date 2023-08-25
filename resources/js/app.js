ed83cde4f82d2ecfbc330e403d796768188c9e32
phucloc217 <loc.nguyen456789@gmail.com>
2023-08-25 16:21:39 +0700

    Khoi tao

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+import {createApp} from 'vue'
+import routes from './router'
+// Plugins
+import GlobalComponents from "./globalComponents";
+import GlobalDirectives from "./globalDirectives";
+import Notifications from "./components/NotificationPlugin";
+
+// MaterialDashboard plugin
+import MaterialDashboard from "./material-dashboard";
+
+import Chartist from "chartist";
+
+import App from './App.vue'
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkExactActiveClass: "nav-item active",
+});
+
+const app = createApp(App)
+app.use(router);
+app.use(VueRouter);
+app.use(MaterialDashboard);
+app.use(GlobalComponents);
+app.use(GlobalDirectives);
+app.use(Notifications);
+app.prototype.$Chartist = Chartist
+app.mount("#app")
