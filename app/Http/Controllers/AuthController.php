e29b8ff7f423c581055379b2a75be7270ad7d320
Phucloc217 <loc.nguyen456789@gmail.com>
2023-09-21 21:43:25 +0700

    new file:   app/Http/Controllers/AuthController.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/api.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'sdt' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['sdt', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'status_code' => 500,
+                    'message' => 'Unauthorized'
+                ]);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Lỗi đăng nhập');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'status_code' => 200,
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $error) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Lỗi đăng nhập',
+                'error' => $error,
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', 'AuthController@login');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('/lophoc', LopHocController::class);
+    Route::resource('/user', UserController::class);
+    Route::resource('/ngayhoc', NgayHocController::class);
+    Route::resource('/monhoc', MonHocController::class);
 });
-
-Route::resource('/lophoc', LopHocController::class);
-Route::resource('/user', UserController::class);
-Route::resource('/ngayhoc',NgayHocController::class);
-Route::resource('/monhoc', MonHocController::class);
